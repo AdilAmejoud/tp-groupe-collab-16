8e0dc5d8f1a1870f160a9971eaf86b708e8d5ae4
MOUAD <mouad.dalilessakali@enim.ac.ma>
2026-03-25 01:23:57 +0100

    feat(calculator): Création de la fonction puissance (#2)

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -5,3 +5,10 @@ def test_add():
 
 def test_subtract():
     assert subtract(5, 2) == 3
+
+from src.calculator import power
+
+def test_power():
+    assert power(2, 3) == 8
+    assert power(5, 0) == 1
+    assert power(2, -1) == 0.5
